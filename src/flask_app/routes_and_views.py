b3ffea90c47fb350a0c782176e26a7b168f9aa41
Gustavo Rondello <gustavorondello99@outlook.com>
2023-01-11 20:55:52 -0300

    fix: render da página job

--- src/flask_app/routes_and_views.py
+++ src/flask_app/routes_and_views.py
@@ -85,7 +85,7 @@ def job(index):
     jobs = read(path="data/jobs.csv")
     selected_job = get_job(jobs, index)
 
-    return render_template("job.junja2", job=selected_job)
+    return render_template("job.jinja2", job=selected_job)
 
 
 def init_app(app: Flask):
