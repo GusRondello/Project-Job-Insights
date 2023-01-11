2cb00231a694080abf9c4a662bee52f4829658e8
Gustavo Rondello <gustavorondello99@outlook.com>
2023-01-11 20:19:07 -0300

    test: teste "test_brazilian_jobs" implementado

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,15 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    excepted_keys = {"title", "salary", "type"}
+    
+    path = "tests/mocks/brazilians_jobs.csv"
+    result = read_brazilian_file(path)
+    
+    keys = set()
+    for job in result:
+        for key in job.keys():
+            keys.add(key)
+    assert keys == excepted_keys
+    
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,6 +3,6 @@ from src.pre_built.counter import count_ocurrences
 
 def test_counter():
     path = "data/jobs.csv"
-    word = "FULL-TIME"
+    word = "python"
     result = count_ocurrences(path, word)
-    assert result == 726
+    assert result == 1639
