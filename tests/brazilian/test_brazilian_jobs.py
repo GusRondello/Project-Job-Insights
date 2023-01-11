a60b6774f8a707de81b32f99f01de22f981937e3
Gustavo Rondello <gustavorondello99@outlook.com>
2023-01-11 20:20:13 -0300

    fix: linhas em branco

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -3,13 +3,12 @@ from src.pre_built.brazilian_jobs import read_brazilian_file
 
 def test_brazilian_jobs():
     excepted_keys = {"title", "salary", "type"}
-    
+
     path = "tests/mocks/brazilians_jobs.csv"
     result = read_brazilian_file(path)
-    
+
     keys = set()
     for job in result:
         for key in job.keys():
             keys.add(key)
     assert keys == excepted_keys
-    
